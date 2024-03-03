4686905028a2655ce76829ffd8cc044c4593fd2d
Jaylin Lee-Sumlin <115357350+Jaylin03@users.noreply.github.com>
2024-03-03 20:52:41 +0000

    Create a password generator. Includes breakdown for flow of the program

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+/*
+Prompt: https://www.practicepython.org/exercise/2014/05/28/16-password-generator.html
+
+Breakdown
+1.) Begin program
+    a.) Ask user for strength of password
+    a.) Generate length of password depending on strength level
+2.) Generate a password
+    a.) Generate a random amount of lowercase letters
+    b.) Generate a random amount of uppercase letters
+    c.) Generate a random amount of numbers
+    d.) Generate a random amount of spacial characters
+    e.) Repeat steps a-d until password length is reached
+3.) Display password
+4.) Ask user if they want to save the password
+    - if yes, save to a file
+    - if no, continue
+5.) Repeat steps 1-4 until user enters "exit"
+*/
+
+
+public class Main {
+  public static void main(String[] args) {
+  }
+}
