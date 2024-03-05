35615a236067f1f40ccee12386c71a8bd877bd4d
Jaylin Lee-Sumlin <115357350+Jaylin03@users.noreply.github.com>
2024-03-05 02:03:17 +0000

    Call the respective functions based on the password strength chosen by the user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ Prompt: https://www.practicepython.org/exercise/2014/05/28/16-password-generator
 Breakdown
 1.) Begin program
     a.) Ask user for strength of password
-    a.) Generate length of password depending on strength level
+    b.) Generate length of password depending on strength level
 2.) Generate a password
     a.) Generate a random amount of lowercase letters
     b.) Generate a random amount of uppercase letters
@@ -18,18 +18,43 @@ Breakdown
 5.) Repeat steps 1-4 until user enters "exit"
 */
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
   public static void main(String[] args) {
+
+    // Step 1a: Ask user for strength of password
     System.out.println("1: Very strong");
     System.out.println("2: Somewhat strong");
-    System.out.println("3: Moderatly strong");
+    System.out.println("3: Moderately strong");
     System.out.println("4: Somewhat weak");
     System.out.println("5: Very weak");
     System.out.print("Select a strength level for this password: ");
-
     Scanner userInput = new Scanner(System.in);
     int passwordStrength = userInput.nextInt();
     userInput.close();
-  }
+
+    Random random = new Random();
+    int passwordLength;
+    // Step 1b: Generate length of password depending on strength level
+    if (passwordStrength == 1) {
+      passwordLength = random.nextInt(15,30);
+      veryStrong(passwordLength);
+    }
+    else if (passwordStrength == 2) {
+      passwordLength = 15;
+      somewhatStrong(passwordLength);
+    }
+    else if (passwordStrength == 3) {
+      passwordLength = 10;
+      moderatelyStrong(passwordLength);
+    }
+    else if (passwordStrength == 4 || passwordStrength == 5) {
+      passwordLength = 5;
+      weak(passwordLength);
+    }
+    else {
+      passwordLength = random.nextInt(15,30);
+      veryStrong(passwordLength);
+    }
 }
